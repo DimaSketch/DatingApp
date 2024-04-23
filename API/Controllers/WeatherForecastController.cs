0c47aee186347d1652393c28ae2fdf0d90d11b11
Dima Dimitriou <mgsdima@gmail.com>
2024-04-23 13:18:04 +0300

    End of section 2

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+// Annotation that gives the controller more functionalities
+[ApiController]
+// Route, framework can direct the http request to the appropriate controller and endpoint
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public WeatherForecastController()
+    {
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    // Adding a [Key] indicates that the variable below is the primary key.
+    public int Id { get; set; }
+
+    // Adding a ? next to the data type, it initiates it as optional.
+    public string UserName {get; set;}
+}
