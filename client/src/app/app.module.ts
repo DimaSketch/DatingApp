5d0709f7563cfdb88d671c19bf6aa67c1b0a052b
Dima Dimitriou <mgsdima@gmail.com>
2024-05-14 16:23:29 +0300

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [ // declare components that the module needs to load
     AppComponent, 
-    NavComponent, HomeComponent, RegisterComponent
+    NavComponent, 
+    HomeComponent, 
+    RegisterComponent, 
+    MemberListComponent, 
+    MemberDetailComponent, 
+    ListsComponent, 
+    MessagesComponent
   ],
   imports: [       // to use any other modules of angular/personal we import them here
     BrowserModule,
@@ -22,7 +31,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
